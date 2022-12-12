8b528fc12b7b81dc31c5944a64691d95ed22e193
Rafaljawad <rafal.qasim@gmail.com>
2022-12-12 17:18:28 -0500

    added unicorn to requirements and deleted unwanted codes

--- app.py
+++ app.py
@@ -5,30 +5,6 @@ import json
 
 app=Flask(__name__)
 
-#restful API function and by passing these parameters so in route will get these parameters through request.form
-#once we get them from request.form will pass them throug  querystring dict
-#then will get json syntax of 3 values , will take just the amount and round it 2 decimal places
-# def get_currency(from_,to_,amt):
-#     url = "https://currency-converter13.p.rapidapi.com/convert"
-#     querystring = {"from":from_,"to":to_,"amount":amt}
-#     headers = {
-#     'x-rapidapi-key': "bbc4e72c95msh764d7107571c9ccp132292jsne000b3620c23",
-#     'x-rapidapi-host': "currency-converter13.p.rapidapi.com"
-#     }
-#     response = requests.request("GET", url, headers=headers, params=querystring).json()
-#     print("***********************",response)
-#     final_result=round(response["amount"],2)
-#     return final_result
-
-
-
-
-# def translat(word,lang):
-#     from googletrans import Translator
-#     translater=Translator()
-#     output=translater.translate(word,dest=lang)
-#     print(output)
-#     return output.text
 def translat(word,source,destination):
     from googletrans import Translator
     translater=Translator()
